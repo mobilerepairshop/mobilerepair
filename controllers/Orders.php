7e8050fe2d4fd33facea716697cdc5da0ec8bdd6
Sarang Barshikar <skbarshikar@mitaoe.ac.in>
2020-11-01 10:29:03 +0530

    myorders backend & responsive done

--- controllers/Orders.php
+++ controllers/Orders.php
@@ -25,7 +25,7 @@
             public function getmyorders($uid)
             {
                 $mc = array();
-                $query = 'select problem,subproblem,mcname,mmodel,created_date,estprice,status,calprice,r.rid,note from requests as r inner join problems as p on r.rid=p.rid where r.uid=? order by r.rid desc';
+                $query = 'select count(problem) as problem,count(subproblem) as subproblem,mcname,mmodel,created_date,estprice,status,calprice,r.rid,note from requests as r inner join problems as p on r.rid=p.rid where r.uid=? group by rid order by r.rid desc';
                 $stmt = $this->conn->prepare($query);
                 $stmt->bind_param('i',$uid);
                 if($stmt->execute())
@@ -46,6 +46,33 @@
                                 "calprice"=>$data["calprice"],
                                 "rid"=>$data["rid"],
                                 "note"=>$data["note"]
+                            ]);
+                    }
+                    return $mc;
+                }
+                else
+                {
+                    return 400;
+                }
+            }
+
+            public function getproblems($uid,$rid)
+            {
+                $mc = array();
+                $query = 'select p.problem,p.subproblem,r.rid from requests as r inner join problems as p on r.rid=p.rid where r.uid=? AND r.rid=?';
+                $stmt = $this->conn->prepare($query);
+                $stmt->bind_param('ii',$uid,$rid);
+                if($stmt->execute())
+                {
+                    $result = $stmt->get_result();   // <--- add this instead
+                    $userinfo = array();
+                    while ($data = $result->fetch_assoc()) 
+                    {
+                        array_push($mc,
+                            [
+                                "problem"=>$data["problem"],
+                                "subproblem"=>$data["subproblem"],
+                                "rid"=>$data["rid"],
                             ]);
                     }
                     return $mc;
