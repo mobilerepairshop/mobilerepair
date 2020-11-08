46ed7914c0acdbb62a91135792cbcabe30d2dd8b
Atharva Deshpande <34884015+AD1310@users.noreply.github.com>
2020-11-08 18:13:28 +0530

    request at delivery boy

---
+++ api/getproblemsforadmin.php
@@ -0,0 +1,25 @@
+<?php 
+
+    require('../controllers/Database.php');
+    require('../controllers/UserAuth.php');
+    require('../controllers/Orders.php');
+
+    $success = $database->connect_db();
+    if($success == '200')
+    {
+        $conn = $database->get_db();
+        
+        $auth = new UserAuth($conn);
+        $id = $_POST['sid'];
+
+        $user = $auth->validateSession($id);
+        if($user[0]=='200')
+        {
+            $orders = new Orders($conn);
+            $rid = $_POST['rid'];
+            // echo "Rid => ".$rid;
+            $values = $orders->getproblemsforadmin($rid);
+            echo json_encode($values);
+        }
+    }
+?>
