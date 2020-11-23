400df6f1eed70f77ff08bce0b503c3896456d261
Atharva Deshpande <34884015+AD1310@users.noreply.github.com>
2020-11-23 09:10:57 +0530

    otp update query modified

--- api/resetpassword.php
+++ api/resetpassword.php
@@ -5,7 +5,7 @@ if(isset($_POST))
     $username = $_POST['username'];
     $pwd = $_POST['pwd'];
     $pwd=md5($pwd);
-    $update = "update users set password='$pwd' where username='$username'";  
+    $update = "update users set password='$pwd' where email='$username'";  
     $run= mysqli_query($con,$update);   
     if($run){
         echo("200");
