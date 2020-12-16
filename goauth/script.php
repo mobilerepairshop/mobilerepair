767c359fa4da6adcc75ec478e33682dc52abf60e
Atharva Deshpande <34884015+AD1310@users.noreply.github.com>
2020-12-16 15:37:05 +0530

    links changed

--- goauth/script.php
+++ goauth/script.php
@@ -15,7 +15,7 @@ $google_client->setClientId('411937557386-8fdmnh35is2oo4bckud4bgmg8okeanql.apps.
 $google_client->setClientSecret('fL8w_8g_bhtzby-YyBgUgkp4');
 
 //Set the OAuth 2.0 Redirect URI
-$google_client->setRedirectUri('http://localhost/mobilerepair/goauth/');
+$google_client->setRedirectUri('./');
 $google_client->setAccessType("offline");
 $google_client->setApprovalPrompt('force');
 //
