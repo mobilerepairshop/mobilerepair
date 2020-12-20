015e9263a1e98f40f9b0a6d76c683bd70291d97b
Atharva Deshpande <34884015+AD1310@users.noreply.github.com>
2020-12-20 13:59:11 +0530

    buttons changed

--- js/login.js
+++ js/login.js
@@ -545,6 +545,8 @@ $.ajax({
               $("#leftnavpane").hide()
               $('[name="editprofile"]').hide();
               $('[name="dashboard"]').hide();
+              $(".float").hide()
+              $(".float2").hide()
 
               $("#contactform-admin").css("display", "block");
               $.ajax({
@@ -566,26 +568,26 @@ $.ajax({
                       str += '<tr><th>Customer Name </th><td>'+para[i].customer+'</td><th>Pickup Date </th><td>'+para[i].date+'</td></tr>'
                       str += '<tr><th>Mobile Company </th><td>'+para[i].mcompany+'</td><th>Pickup Time </th><td>'+para[i].time+'</td></tr>'
                       str += '<tr><th>Mobile Model </th><td>'+para[i].mmodel+'</td><th>Problems </th><td><a style="color:blue;" data-toggle="modal" data-target="#problem" onclick="getproblems_admin('+para[i].rid+')">Problems</a></td></tr>'
-                      str += '<tr><th>Customer Address </th><td>'+para[i].address+'</td><th>Customer Contact </th><td>'+para[i].phonenum+'</td></tr>'
+                      str += '<tr><th>Customer Address </th><td>'+para[i].address+'</td><th>Customer Contact </th><td><a href="tel:'+para[i].phonenum+'">'+para[i].phonenum+'</td></tr>'
                       if(para[i].delivery_status==1 && para[i].pay_method=="cod" && para[i].pay_status=="0")
                       {
-                        str += '</tbody></table></div><div class="col-md-3"><br><button id="'+para[i].rid+'" class="button btn-success button-small button-rouded" onclick="paid(this.id,'+para[i].status+')">Amount Paid</button></div><div class="col-md-4"><br><button id="'+para[i].rid+'" class="button btn-success button-small button-rouded" onclick="pickedup(this.id,'+para[i].status+')" '+disable_drop+'>'+status[para[i].status]+'</button></div></div></div></div>'
+                        str += '</tbody></table></div><div class="col-md-3"><br><button id="'+para[i].rid+'" class="button btn-success button-small " onclick="paid(this.id,'+para[i].status+')">Amount Paid</button></div><div class="col-md-4"><br><button id="'+para[i].rid+'" class="button btn-success button-small " onclick="pickedup(this.id,'+para[i].status+')" '+disable_drop+'>'+status[para[i].status]+'</button></div></div></div></div>'
                       }
                       else if(para[i].delivery_status==1 && para[i].pay_status=="1")
                       {
-                        str += '</tbody></table></div><div class="col-md-4"><br><button id="'+para[i].rid+'" class="button btn-success button-small button-rouded" onclick="pickedup(this.id,'+para[i].status+')">'+status[para[i].status]+'</button></div></div></div></div>'
+                        str += '</tbody></table></div><div class="col-md-4"><br><button id="'+para[i].rid+'" class="button btn-success button-small " onclick="pickedup(this.id,'+para[i].status+')">'+status[para[i].status]+'</button></div></div></div></div>'
                       }
                       else if(para[i].delivery_status==0 && para[i].status==1)
                       {
-                        str += '</tbody></table></div><div class="col-md-4"><br><button id="'+para[i].rid+'" class="button btn-success button-small button-rouded" data-toggle="modal" data-target="#verifyuserdelivery" data-backdrop="static" data-keyboard="false" onclick="SendOTPtoUser(this.id,'+para[i].status+')" name="sendotpbutton">Send OTP</button></div><div class="col-md-4"><br><button id="'+para[i].rid+'" class="button btn-success button-small button-rouded" onclick="pickedup(this.id,'+para[i].status+')">'+status[para[i].status]+'</button></div><div class="col-md-4"><br><button id="'+para[i].rid+'" class="button btn-danger button-small button-rouded" onclick="cancelled(this.id,'+para[i].status+')">Cancelled</button></div></div></div></div>'
+                        str += '</tbody></table></div><div class="col-md-4"><br><button id="'+para[i].rid+'" class="button btn-success button-small " data-toggle="modal" data-target="#verifyuserdelivery" data-backdrop="static" data-keyboard="false" onclick="SendOTPtoUser(this.id,'+para[i].status+')" name="sendotpbutton">Send OTP</button></div><div class="col-md-4"><br><button id="'+para[i].rid+'" class="button btn-success button-small " onclick="pickedup(this.id,'+para[i].status+')">'+status[para[i].status]+'</button></div><div class="col-md-4"><br><button id="'+para[i].rid+'" class="button btn-danger button-small " onclick="cancelled(this.id,'+para[i].status+')">Cancelled</button></div></div></div></div>'
                       }
                       else if(para[i].delivery_status==1 && para[i].status==8)
                       {
-                        str += '</tbody></table></div><div class="col-md-4"><br><button id="'+para[i].rid+'" class="button btn-success button-small button-rouded" data-toggle="modal" data-target="#verifyuserdelivery" data-backdrop="static" data-keyboard="false" onclick="SendOTPtoUser(this.id,'+para[i].status+')" name="sendotpbutton">Send OTP</button></div><div class="col-md-4"><br><button id="'+para[i].rid+'" class="button btn-success button-small button-rouded" onclick="pickedup(this.id,'+para[i].status+')">'+status[para[i].status]+'</button></div></div></div></div>'
+                        str += '</tbody></table></div><div class="col-md-4"><br><button id="'+para[i].rid+'" class="button btn-success button-small " data-toggle="modal" data-target="#verifyuserdelivery" data-backdrop="static" data-keyboard="false" onclick="SendOTPtoUser(this.id,'+para[i].status+')" name="sendotpbutton">Send OTP</button></div><div class="col-md-4"><br><button id="'+para[i].rid+'" class="button btn-success button-small " onclick="pickedup(this.id,'+para[i].status+')">'+status[para[i].status]+'</button></div></div></div></div>'
                       }
                       else
                       {
-                        str += '</tbody></table></div><div class="col-md-4"><br><button id="'+para[i].rid+'" class="button btn-success button-small button-rouded" onclick="pickedup(this.id,'+para[i].status+')">'+status[para[i].status]+'</button></div></div></div></div>'
+                        str += '</tbody></table></div><div class="col-md-4"><br><button id="'+para[i].rid+'" class="button btn-success button-small " onclick="pickedup(this.id,'+para[i].status+')">'+status[para[i].status]+'</button></div></div></div></div>'
                       }
                     }
                     $(".mycards").append(str) 
